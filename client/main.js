6def43a2dd0a0b505280a4c2e2678bc7557035a0
Ropah <ropah.musvaire@gmail.com>
2018-09-11 11:05:45 +0000

    Add form for new tasks

--- client/main.js
+++ client/main.js
@@ -1,1 +1,12 @@
 import '../imports/ui/body.js';
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './body.html';
+
+// Template.body.helpers({
+//   tasks() {
+//     return Tasks.find({});
+//   },
+// });
