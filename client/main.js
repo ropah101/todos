a9f781c3d43be85a68a49082adfb9a1b0c484348
Ropah <ropah.musvaire@gmail.com>
2018-09-12 21:21:10 +0000

    Add buttons to Task componen

--- client/main.js
+++ client/main.js
@@ -1,12 +1,4 @@
 import '../imports/ui/body.js';
-import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
 
-import './body.html';
-
-// Template.body.helpers({
-//   tasks() {
-//     return Tasks.find({});
-//   },
-// });
